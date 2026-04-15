34acc8cd798f04389e23457e350608db56fe5c3a
David Serrano <jesusdavidserrano.a@gmail.com>
2026-04-14 19:30:00 -0500

    Eliminando carpeta duplicada

--- Proyecto-trazabilidad-de-productos-agricolas-de-exportaci-n/app/static/js/app.js
+++
@@ -1,9 +0,0 @@
-function probarAPI() {
-    fetch("http://127.0.0.1:5000/")
-        .then(res => res.json())
-        .then(data => {
-            document.getElementById("resultado").textContent =
-                JSON.stringify(data, null, 2);
-        })
-        .catch(err => console.log(err));
-}
