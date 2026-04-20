486c370616709769088a5460c492617d3b20d7d2
David Serrano <jesusdavidserrano.a@gmail.com>
2026-04-20 18:08:36 -0500

    agregando el modelo de cosechas

---
+++ app/models/cosecha.py
@@ -0,0 +1,17 @@
+from app.extensions import db
+from datetime import datetime
+
+
+class Cosecha(db.Model):
+    __tablename__ = 'cosechas'
+
+    id                = db.Column(db.Integer, primary_key=True)
+    lote_id           = db.Column(db.Integer, db.ForeignKey('lotes.id'),    nullable=False)
+    fecha_cosecha     = db.Column(db.Date, nullable=False)
+    cantidad_total_kg = db.Column(db.Numeric(10, 2))
+    observaciones     = db.Column(db.Text)
+    usuario_id        = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    fecha_creacion    = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Cosecha lote={self.lote_id} kg={self.cantidad_total_kg}>'
